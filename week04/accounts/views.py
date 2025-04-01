97cd9a56b5b42c2631452cf1884a2a205cb0fbc2
bluebomin <bluebomin@sookmyung.ac.kr>
2025-04-01 21:22:04 +0900

    메인페이지 추가

--- week04/accounts/views.py
+++ week04/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 def signup(request):
     if request.method == "POST":
@@ -8,7 +8,6 @@ def signup(request):
         if form.is_valid():
             form.save()
             return redirect("login")
-
     else:
         form = SignupForm()
 
@@ -25,3 +24,11 @@ def login(request):
             return redirect("main")
     
     return render(request, "login.html")
+
+def main(request):
+    return render(request, "main.html")
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+        return redirect("login")
