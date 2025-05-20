3b879346743e5c04f1be9702650524b4065dbebb
bluebomin <bluebomin@sookmyung.ac.kr>
2025-05-20 20:04:38 +0900

    Feat : @api-view+request body 계산기 구현

--- django/week04/posts/views_cal.py
+++ django/week04/posts/views_cal.py
@@ -1,11 +1,9 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def calculator_query(request):
-    num1 = request.GET.get('num1', 0)
-    num2 = request.GET.get('num2', 0)
-    op = request.GET.get('op')
+@api_view(['POST'])
+def calculator_body(request):
+    data = request.data
+    num1 = data.get('num1', 0)
+    num2 = data.get('num2', 0)
+    op=data.get('op')
 
     if op == '+':
         result = int(num1) + int(num2)
