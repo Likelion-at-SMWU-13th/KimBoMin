3f8a54b31e13bc210de21c002e459d875982ed0f
bluebomin <bluebomin@sookmyung.ac.kr>
2025-05-22 04:42:42 +0900

    FEAT : 서버 열기

--- django/week04_hw/users/serializers.py
+++ django/week04_hw/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import Post  
 
 User = get_user_model()
 
@@ -13,3 +14,9 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
