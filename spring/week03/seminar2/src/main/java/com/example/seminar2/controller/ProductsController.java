e27f444f24c9cc86a8a95e88d4c2b1edf21c6caa
bluebomin <bluebomin@sookmyung.ac.kr>
2025-07-29 20:20:44 +0900

    수정

--- spring/week03/seminar2/src/main/java/com/example/seminar2/controller/ProductsController.java
+++ spring/week03/seminar2/src/main/java/com/example/seminar2/controller/ProductsController.java
@@ -1,21 +1,43 @@
 package com.example.seminar2.controller;
 
+import com.example.seminar2.model.Product;
 import com.example.seminar2.service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class ProductsController {
-    
-    private ProductService productionService;
-    
-    public ProductsController(ProductService productService){
-        this.productionService = productService;
+
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
     }
-    
-    @RequestMapping("/products")
-    public String viewProducts(Model model){
-        var products = productionService.findAll();
+
+    @GetMapping("/products")
+    public String viewProducts(Model model) {
+        var products = productService.findAll();
         model.addAttribute("products", products);
 
         return "products.html";
     }
+
+    @PostMapping(path="/products")
+    public String addProduct(
+            @RequestParam String name,
+            @RequestParam double price,
+            Model model
+    ) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        productService.addProduct(p);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
 }
