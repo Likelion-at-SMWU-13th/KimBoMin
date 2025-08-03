3f1925e39abad690966c0d82a4ceaf24b56114e0
bluebomin <bluebomin@sookmyung.ac.kr>
2025-08-03 20:09:07 +0900

    [FEAT] Book.java 생성

--- spring/week03-hw/seminar-hw/src/main/java/com/example/seminarhw/controller/MainController.java
+++ spring/week03-hw/seminar-hw/src/main/java/com/example/seminarhw/controller/MainController.java
@@ -1,13 +1,27 @@
 package com.example.seminarhw.controller;
 
+import com.example.seminarhw.model.Book;
+import com.example.seminarhw.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class MainController {
+public class BookController {
 
-    @RequestMapping("/books")
-    public String books(){
-        return "books.html";
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books")
+    public String books(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+
+    @PostMapping("/books")
+    public String addBook(@ModelAttribute Book book) {
+        bookService.addBook(book);
+        return "redirect:/books";
     }
 }
