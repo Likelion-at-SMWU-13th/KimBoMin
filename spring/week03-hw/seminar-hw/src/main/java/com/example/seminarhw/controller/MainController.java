161b35736a9ebc6590225ddb648a039df671dbe7
bluebomin <bluebomin@sookmyung.ac.kr>
2025-08-03 20:15:02 +0900

    [FIX] books.html 리팩토링

--- spring/week03-hw/seminar-hw/src/main/java/com/example/seminarhw/controller/MainController.java
+++ spring/week03-hw/seminar-hw/src/main/java/com/example/seminarhw/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class BookController {
+public class MainController {
 
     @Autowired
     private BookService bookService;
