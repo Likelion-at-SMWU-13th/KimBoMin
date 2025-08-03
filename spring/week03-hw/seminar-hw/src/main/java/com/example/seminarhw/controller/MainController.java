5d9994f81493e779615017cd1a0d3b7e788d04db
bluebomin <bluebomin@sookmyung.ac.kr>
2025-08-03 17:54:45 +0900

    [FEAT] MainController 생성

---
+++ spring/week03-hw/seminar-hw/src/main/java/com/example/seminarhw/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.seminarhw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/books")
+    public String books(){
+        return "books";
+    }
+}
