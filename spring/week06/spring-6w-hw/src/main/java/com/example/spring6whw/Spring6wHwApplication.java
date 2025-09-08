84363d7e01916d1ff59ca219993085dbce0a3bd5
bluebomin <bluebomin@sookmyung.ac.kr>
2025-09-09 02:19:49 +0900

    setting

---
+++ spring/week06/spring-6w-hw/src/main/java/com/example/spring6whw/Spring6wHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring6whw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6wHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6wHwApplication.class, args);
+    }
+
+}
