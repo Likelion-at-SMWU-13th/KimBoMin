abd3e0dadec860102bf604b78e187e1cd696d702
김보민 <157809900+bluebomin@users.noreply.github.com>
2025-09-14 12:21:33 +0900

    프로젝트 ì생성

---
+++ spring/week07/springSeminar-week07hw/src/test/java/com/example/springseminarweek07hw/SpringSeminarWeek07hwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springseminarweek07hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeminarWeek07hwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
