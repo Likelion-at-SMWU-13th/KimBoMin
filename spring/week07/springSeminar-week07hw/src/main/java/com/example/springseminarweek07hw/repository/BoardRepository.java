abf6dd47032bb7e332a7daecf69f6abc487db03a
김보민 <157809900+bluebomin@users.noreply.github.com>
2025-09-14 12:32:44 +0900

    레포지토리 생성

--- spring/week07/springSeminar-week07hw/src/main/java/com/example/springseminarweek07hw/repository/BoardRepository.java
+++ spring/week07/springSeminar-week07hw/src/main/java/com/example/springseminarweek07hw/repository/BoardRepository.java
@@ -1,4 +1,6 @@
 package com.example.springseminarweek07hw.repository;
 
-public class BoardRepository {
-}
+import com.example.springseminarweek07hw.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {}
--- spring/week07/springSeminar-week07hw/src/main/java/com/example/springseminarweek07hw/repository/MemberRepository.java
+++ spring/week07/springSeminar-week07hw/src/main/java/com/example/springseminarweek07hw/repository/MemberRepository.java
@@ -1,4 +1,6 @@
 package com.example.springseminarweek07hw.repository;
 
-public class MemberRepository {
-}
+import com.example.springseminarweek07hw.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {}
