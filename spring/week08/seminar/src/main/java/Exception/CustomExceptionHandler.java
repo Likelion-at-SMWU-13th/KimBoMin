f974ba3080868a6f25100fb13d33892df9b5e08c
김보민 <157809900+bluebomin@users.noreply.github.com>
2025-09-16 19:33:18 +0900

    ìFix : mapping error

--- spring/week08/seminar/src/main/java/Exception/CustomExceptionHandler.java
+++ spring/week08/seminar/src/main/java/Exception/CustomExceptionHandler.java
@@ -1,4 +1,39 @@
 package Exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class CustomExceptionHandler {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class);
+
+
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<Map<String, String>> handleException(CustomException e, HttpServletRequest request) {
+        LOGGER.error("Advice 내 handleException 호출, URI: {}, 메시지: {}",
+                request.getRequestURI(), e.getMessage());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", e.getHttpStatus().getMessage());
+        map.put("code", Integer.toString(e.getHttpStatus().getCode()));
+        map.put("message", e.getMessage());
+
+        return ResponseEntity
+                .status(e.getHttpStatus().getCode())
+                .body(map);
+    }
+
+
 }
