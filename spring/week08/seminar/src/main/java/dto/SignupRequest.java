2393b7d5d41608c29d7bcd17e9d0ae52601c280f
김보민 <157809900+bluebomin@users.noreply.github.com>
2025-09-21 00:19:19 +0900

    Fix : dto

---
+++ spring/week08/seminar/src/main/java/dto/SignupRequest.java
@@ -0,0 +1,28 @@
+package com.example.seminar.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class SignupRequest {
+
+    @NotBlank(message = "아이디는 공백일 수 없습니다.")
+    private String username;
+
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 공백일 수 없습니다.")
+    private String email;
+
+    @Size(min = 8, message = "비밀번호는 최소 8자리 이상이어야 합니다.")
+    private String password;
+
+    // getter, setter
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
